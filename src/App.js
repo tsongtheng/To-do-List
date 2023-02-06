07f73fe8eb7f94823f3917dabc4c024bba2e90ea
Tipchan Songtheng <tipchansongtheng123@gmail.com>
2023-02-06 10:22:31 +0530

    New components added and updated

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import React, { useState } from "react";
 import ToDoItem from "./components/ToDoItem";
+import InputArea from "./components/InputArea";
+
 function App() {
-  const [inputText, setInputText] = useState("");
   const [items, setItems] = useState([]);
 
-  function handleChange(event) {
-    const newValue = event.target.value;
-    setInputText(newValue);
-  }
-
-  function addItem() {
+  function addItem(inputText) {
     setItems((prevItems) => {
       return [...prevItems, inputText];
     });
-    setInputText("");
   }
 
   function deleteItem(id) {
@@ -27,12 +22,7 @@ function App() {
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
-      <div className="form">
-        <input onChange={handleChange} type="text" value={inputText} />
-        <button onClick={addItem}>
-          <span>Add</span>
-        </button>
-      </div>
+      <InputArea onAdd={addItem} />
       <div>
         <ul>
           {items.map((todoItem, index) => (
